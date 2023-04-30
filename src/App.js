24f289212c3904e833a05f81867690c803492a94
brayanck <brayanmampaso@gmail.com>
2023-04-30 18:19:31 -0300

    solo boostrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Componentes from './componentes/Componentes';
+
 import './App.css';
 import Carrusel from './componentes/Carrusel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
 	return (
     <div className='bg'>
-      <Componentes/>
     <Carrusel/>
     </div>
 		
